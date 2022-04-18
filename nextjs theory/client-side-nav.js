e4814850f7ab02826a94abeaf6e8d115dd17cc02
svanidzee <svanidzeee@gmail.com>
2022-04-18 06:53:04 +0100

    create login api

--- nextjs theory/client-side-nav.js
+++
@@ -1,6 +0,0 @@
-// The Link component enables client-side navigation between two pages in the same Next.js app
-
-// Client-side navigation means that the page transition happens using JavaScript, which
-// is faster than the default navigation done by the browser
-
-// If you need to link to an external page outside the Next.js app, just use an <a> tag without Link
